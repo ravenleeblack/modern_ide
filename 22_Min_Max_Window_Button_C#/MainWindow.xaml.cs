d0bb7d436df21132bf180ea437986a2db480f290
Robert Nelson <lordravenblack36@gmail.com>
2023-10-12 21:29:43 -0500

    Update MainWindow.xaml.cs

--- 22_Min_Max_Window_Button_C#/MainWindow.xaml.cs
+++ 22_Min_Max_Window_Button_C#/MainWindow.xaml.cs
@@ -1,1 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
 
+
+namespace Illeshian_Ide_
+{
+    public partial class MainWindow : Window
+    {
+        private compiler_manager CompilerManager { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CompilerManager = new compiler_manager(status_bar, status_text);
+        }
+
+        public void run_program(object sender, RoutedEventArgs e)
+        {
+            CompilerManager.run_program(sender, e);
+        }
+
+        public void stop_program(object sender, RoutedEventArgs e)
+        {
+            CompilerManager.stop_program(sender, e);
+        }
+
+        private void exit_program(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void min_program(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void max_program(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+    }
+}
