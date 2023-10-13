ced1f4efb10cf4b9f1d650a8a5e306a2dac8e681
Robert Nelson <lordravenblack36@gmail.com>
2023-10-13 00:26:57 -0500

    Update main_menu.cs

--- 23_File_Button_Menu_C#/main_menu.cs
+++ 23_File_Button_Menu_C#/main_menu.cs
@@ -1,1 +1,31 @@
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
 
+
+namespace Illeshian_Ide_
+{
+    public partial class main_menu : Window
+    {
+        private MainWindow mainWindow;
+        private Label file_label;
+
+        public main_menu(MainWindow mainWindow, Label fileLabel)
+        {
+            this.mainWindow = mainWindow;
+            this.file_label = fileLabel;
+        }
+
+        public void file_program(object sender, RoutedEventArgs e)
+        {
+            file_label.Visibility = Visibility.Visible;
+        }
+    }
+}
